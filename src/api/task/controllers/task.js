f61534d6e2ecdcaaccd631b7da7fb1d26db33a4e
denisz98 <deniszaldivarperez98@gmail.com>
2023-11-01 12:23:57 -0400

    first commit

---
+++ src/api/task/controllers/task.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * task controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::task.task');
+
+
+
